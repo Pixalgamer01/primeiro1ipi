9d169237b33b24ed898caf3a5e454cee9a7af6c5
pixalgamer01 <kuramagames822@gmail.com>
2023-09-13 17:04:11 -0300

    menu principal

---
+++ java.js
@@ -0,0 +1,29 @@
+var lista_de_produtos = []
+
+
+
+while(true){
+
+var opcao = prompt(`
+
+ bem vindo ao cadastro de produtos :)
+
+ 1. cadastrar produtos
+ 2. editar produtos
+ 3. remover produtos
+ 4. visualizar todos os produtos
+`)
+
+}
+if(opcao == 1){
+ // codigo para adicionar o produto
+
+}else if (opcao == 2){
+// codigo para editar o produto{
+}else if (opcao == 3){
+//codigo para remover produto
+}else if (opcao == 4){
+    //visalizar produto
+}else{
+    alert("nenhuma das opções foram seleciondas")
+}
